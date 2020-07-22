1a952dd2e4bc10367f6550f14a837085994a792f
Danilo Augusto <dasf_cm@terra.com.br>
2020-07-22 00:31:09 -0300

    Create core first configs

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import eventex.core.views
+
 urlpatterns = [
+    path("", eventex.core.views.home),
     path("admin/", admin.site.urls),
 ]
