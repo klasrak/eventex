a94990e26fb7a5bafe4387978a2b9b7c0c9c95ac
Danilo Augusto <dasf_cm@terra.com.br>
2020-07-22 03:00:50 -0300

    Add subscription app and first test

---
+++ eventex/subscriptions/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+
+class SubscribeTest(TestCase):
+    def test_get(self):
+        """GET /inscricao/ must return status code 200"""
+        response = self.client.get("/inscricao/")
+        self.assertEqual(200, response.status_code)
