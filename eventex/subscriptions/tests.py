f9b7e15da3afba30b6331c278c6f2fc01a22aee4
Danilo Augusto <dasf_cm@terra.com.br>
2020-07-22 03:11:37 -0300

    Add test to assert subscription form template

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -2,7 +2,13 @@ from django.test import TestCase
 
 
 class SubscribeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get("/inscricao/")
+
     def test_get(self):
         """GET /inscricao/ must return status code 200"""
-        response = self.client.get("/inscricao/")
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use subscriptions/subscriptions_form.html"""
+        self.assertTemplateUsed(self.response, "subscriptions/subscription_form.html")
--- eventex/urls.py
+++ eventex/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from eventex.core.views import home
+from eventex.subscriptions.views import subscribe
 
 urlpatterns = [
     path("", home),
+    path("inscricao/", subscribe),
     path("admin/", admin.site.urls),
 ]
