823cdaa3b64ad25b57cb36c32a1c724670a39e11
Danilo Augusto <dasf_cm@terra.com.br>
2020-08-24 19:23:59 -0300

    Add tests for subscriptions

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -12,3 +12,15 @@ class SubscribeTest(TestCase):
     def test_template(self):
         """Must use subscriptions/subscriptions_form.html"""
         self.assertTemplateUsed(self.response, "subscriptions/subscription_form.html")
+
+    def test_html(self):
+        """Html must contain input tags"""
+        self.assertContains(self.response, "<form")
+        self.assertContains(self.response, "<input", 6)
+        self.assertContains(self.response, 'type="text"', 3)
+        self.assertContains(self.response, 'type="email"')
+        self.assertContains(self.response, 'type="submit"')
+
+    def test_csrf(self):
+        """Html must contain csrf"""
+        self.assertContains(self.response, "csrfmiddlewaretoken")
