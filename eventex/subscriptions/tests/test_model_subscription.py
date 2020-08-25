6c0e3a79a15689333171874a543a76656de0996d
Danilo Augusto <dasf_cm@terra.com.br>
2020-08-25 00:32:58 -0300

    Add subscription created at field and test

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -1,13 +1,20 @@
+from datetime import datetime
+
 from django.test import TestCase
 
 from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelTest(TestCase):
-    def test_create(self):
-        obj = Subscription(
+    def setUp(self):
+        self.obj = Subscription(
             name="Danilo Augusto", cpf="12345678901", email="daniloaugusto@mailinator.com", phone="18-91111-2222"
         )
-        obj.save()
+        self.obj.save()
 
+    def test_create(self):
         self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
