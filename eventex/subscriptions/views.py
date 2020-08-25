f0316966150aed13c426f0e758f8f658edb21785
Danilo Augusto <dasf_cm@terra.com.br>
2020-08-25 00:40:33 -0300

    Add Subscription model tests and migrations

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -32,6 +33,9 @@ def create(request):
         "subscriptions/subscription_email.txt",
         form.cleaned_data,
     )
+
+    # Create
+    Subscription.objects.create(**form.cleaned_data)
 
     # Success feedback
     messages.success(request, "Inscrição realizada com sucesso!")
