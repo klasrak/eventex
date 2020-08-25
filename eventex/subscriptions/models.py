5d841347bea3377d17c740c58f28dd159af12c6c
Danilo Augusto <dasf_cm@terra.com.br>
2020-08-25 00:29:09 -0300

    Add Subscription model

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 
 class Subscription(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
