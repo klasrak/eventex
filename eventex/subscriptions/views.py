747b4c6e04b18ae0497d9ef9c19fcd0de49b440f
Danilo Augusto <dasf_cm@terra.com.br>
2020-08-24 23:10:15 -0300

    Refactor subscriptions view

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import messages
 from django.core import mail
 from django.http import HttpResponseRedirect
@@ -8,33 +9,40 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
-
     if request.method == "POST":
-
-        form = SubscriptionForm(request.POST)
-
-        if form.is_valid():
-            form.full_clean()
-
-            body = render_to_string("subscriptions/subscription_email.txt", form.cleaned_data)
-
-            mail.send_mail(
-                "Confirmação de Inscrição",
-                body,
-                "contato@eventex.com.br",
-                ["contato@eventex.com.br", form.cleaned_data["email"]],
-            )
-
-            messages.success(request, "Inscrição realizada com sucesso!")
-            return HttpResponseRedirect("/inscricao/")
-
-        else:
-            return render(request, "subscriptions/subscription_form.html", {"form": form})
-
+        return create(request)
     else:
+        return new(request)
 
-        context = {
-            "form": SubscriptionForm(),
-        }
 
-        return render(request, "subscriptions/subscription_form.html", context)
+def create(request):
+
+    form = SubscriptionForm(request.POST)
+
+    if not form.is_valid():
+        return render(request, "subscriptions/subscription_form.html", {"form": form})
+
+    form.full_clean()
+
+    # Send email
+    _send_mail(
+        "Confirmação de Inscrição",
+        settings.DEFAULT_FROM_EMAIL,
+        form.cleaned_data["email"],
+        "subscriptions/subscription_email.txt",
+        form.cleaned_data,
+    )
+
+    # Success feedback
+    messages.success(request, "Inscrição realizada com sucesso!")
+
+    return HttpResponseRedirect("/inscricao/")
+
+
+def new(request):
+    return render(request, "subscriptions/subscription_form.html", {"form": SubscriptionForm()})
+
+
+def _send_mail(subject, from_, to, template_name, context):
+    body = render_to_string(template_name, context)
+    mail.send_mail(subject, body, from_, [from_, to])
