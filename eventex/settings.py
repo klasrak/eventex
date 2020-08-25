7a5b6a004ff912bf38d503103144936a158d7632
Danilo Augusto <dasf_cm@terra.com.br>
2020-08-24 21:29:38 -0300

    Add smtp email server with sendinblue

--- eventex/settings.py
+++ eventex/settings.py
@@ -116,3 +116,13 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Email configuration
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" to use in dev
+
+EMAIL_BACKEND = config("EMAIL_BACKEND")
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
